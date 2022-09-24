59d3b763a2459b5d6229baaf13c32dad23aed1c5
Your Name <you@example.com>
2022-09-24 17:58:44 +0700

    Xay dung trang gio hang

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,10 @@
 import 'package:flutter/material.dart';
+import 'package:myshop/ui/cart/cart_screen.dart';
 import 'package:myshop/ui/products/user_products_screen.dart';
 import 'ui/products/products_manager.dart';
 import 'ui/products/product_detail_screen.dart';
 import 'ui/products/product_overview_screen.dart';
+import 'ui/cart/cart_screen.dart';
 
 void main() {
   runApp(const MyApp());
@@ -27,9 +29,8 @@ class MyApp extends StatelessWidget {
         ),
 
       home: const SafeArea (
-        child: UserProductsScreen(),
+        child: CartScreen(),
       ),
     );
   }
-
 }
