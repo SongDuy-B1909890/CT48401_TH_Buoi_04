5aa33c287f2b540038af1c770610932029858159
Your Name <you@example.com>
2022-10-21 19:16:22 +0700

    Cai dat chuc nang chon san pham yeu thich

--- lib/ui/products/product_grid_tile.dart
+++ lib/ui/products/product_grid_tile.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import '../../models/product.dart';
 import 'product_detail_screen.dart';
@@ -35,14 +36,20 @@ class ProductGridTile extends StatelessWidget {
     Widget buildGridFooterBar(BuildContext context) {
       return GridTileBar(
         backgroundColor: Colors.black87,
-        leading:  IconButton(
-          icon: Icon(
-            product.isFavorite ? Icons.favorite : Icons.favorite_border,
-          ),
-          color: Theme.of(context).colorScheme.secondary,
-          onPressed: () {
-            print('Toggle a favorite product');
+        leading:  ValueListenableBuilder<bool>(
+          valueListenable: product.isFavoriteListenable,
+          builder: (ctx, isFavorite, child) {
+            return IconButton(
+              icon: Icon(
+                isFavorite ? Icons.favorite : Icons.favorite_border,
+              ),
+              color: Theme.of(context).colorScheme.secondary,
+              onPressed: () {
+                product.isFavorite = !isFavorite;
+              },
+            );
           },
+  
         ),
         title: Text(
           product.title,
